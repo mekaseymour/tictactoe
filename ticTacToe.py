97896a5f8549a3c5a6a9fd750d849fae6e0b0677
Meka <mekaseymour@gmail.com>
2017-01-04 13:59:57 -0500

    changed place names to 1-9, added pick player functionality

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,38 +1,41 @@
-theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
-            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
-            'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
+# Simple tic tac toe logic and board
+
+theBoard = {1: ' ', 2: ' ', 3: ' ',
+            4: ' ', 5: ' ', 6: ' ',
+            7: ' ', 8: ' ', 9: ' '}
 
 def printBoard(board):
-    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print(board[1] + '|' + board[2] + '|' + board[3])
     print('-+-+-')
-    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print(board[4] + '|' + board[5] + '|' + board[6])
     print('-+-+-')
-    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+    print(board[7] + '|' + board[8] + '|' + board[9])
 
-turn = 'X'
+def playGame():
+    player = input("Which player would you like to be (X or O)?")
+    turn = player.upper()
 
-for i in range(9):
-    printBoard(theBoard)
-    print('Turn for ' + turn + '. Move on which space?')
-    move = input()
-    theBoard[move] = turn
-    #gameWin(theBoard, turn)
+    for i in range(9):
+        printBoard(theBoard)
+        print('Turn for ' + turn + '. Move on which space?')
+        move = int(input())
+        theBoard[move] = turn
+        #gameWin(theBoard, turn)
 
-    if ((theBoard['top-L'] == turn and theBoard['top-M'] == turn and theBoard['top-R'] == turn) or
-    (theBoard['mid-L'] == turn and theBoard['mid-M'] == turn and theBoard['mid-R'] == turn) or
-    (theBoard['low-L'] == turn and theBoard['low-M'] == turn and theBoard['low-R'] == turn) or
-    (theBoard['top-L'] == turn and theBoard['mid-L'] == turn and theBoard['low-L'] == turn) or
-    (theBoard['top-M'] == turn and theBoard['mid-M'] == turn and theBoard['low-M'] == turn) or
-    (theBoard['top-R'] == turn and theBoard['mid-R'] == turn and theBoard['low-R'] == turn) or
-    (theBoard['top-L'] == turn and theBoard['mid-M'] == turn and theBoard['low-R'] == turn) or # wins diagonally
-    (theBoard['top-R'] == turn and theBoard['mid-M'] == turn and theBoard['low-L'] == turn)):
-        print ("\nPlayer %s wins!!!!\n" % turn)
-        break
-    else:
-        if turn == 'X':
-            turn = 'O'
+        if ((theBoard[1] == turn and theBoard[2] == turn and theBoard[3] == turn) or # wins across the top
+        (theBoard[4] == turn and theBoard[5] == turn and theBoard[6] == turn) or # wins across the middle row
+        (theBoard[7] == turn and theBoard[8] == turn and theBoard[9] == turn) or # wins across the bottom
+        (theBoard[1] == turn and theBoard[4] == turn and theBoard[7] == turn) or # wins down the left column
+        (theBoard[2] == turn and theBoard[5] == turn and theBoard[8] == turn) or # wins down the middle column
+        (theBoard[3] == turn and theBoard[6] == turn and theBoard[9] == turn) or # wins down the right column
+        (theBoard[1] == turn and theBoard[5] == turn and theBoard[9] == turn) or # wins diagonally
+        (theBoard[3] == turn and theBoard[5] == turn and theBoard[7] == turn)): # wins diagonally
+            print ("\nPlayer %s wins!!!!\n" % turn)
+            break
         else:
-            turn = 'X'
+            if turn == 'X':
+                turn = 'O'
+            else:
+                turn = 'X'
 
-
-#printBoard(theBoard)
+playGame()
