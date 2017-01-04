671de68527a137ec3c98568f47e00f2d09c826ee
Meka <mekaseymour@gmail.com>
2017-01-04 16:13:27 -0500

    added inital player selection check, program plays against user, random selection of who goes first, re-prompts if board space is already filled

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,4 +1,5 @@
 # Simple tic tac toe logic and board
+from random import randint
 
 theBoard = {1: ' ', 2: ' ', 3: ' ',
             4: ' ', 5: ' ', 6: ' ',
@@ -12,15 +13,45 @@ def printBoard(board):
     print(board[7] + '|' + board[8] + '|' + board[9])
 
 def playGame():
-    player = input("Which player would you like to be (X or O)?")
-    turn = player.upper()
+    # ask user to choose to be player 'X' or 'O'
+    player_one = input("Which player would you like to be (X or O)?: ")
+    player_one = player_one.upper()
+
+    # check that the user's entry is either 'X' or 'O'
+    while player_one != 'X' and player_one != 'O':
+        player_one = input("Enter either X or O to start.: ")
+        player_one = player_one.upper()
+
+    print("OK! Let's play!\n")
+    printBoard(theBoard)
+
+    # randomly pick which player goes first
+    choice = randint(1,2)
+    if choice == 1:
+        turn = 'X'
+    else:
+        turn = 'O'
 
     for i in range(9):
+        #print('Turn for ' + turn + '. Move on which space?')
+        print('Turn for ' + turn + '.')
+        if turn != player_one:
+            # computer plays
+            computer_pick = randint(1,9)
+            # picks new random position if that board space already contains an 'X' or 'O'
+            while theBoard[computer_pick] != ' ':
+                computer_pick = randint(1,9)
+            theBoard[computer_pick] = turn
+        else:
+            # user plays
+            print('Move on which space?')
+            move = int(input())
+            # re-promppts user if that board space already contains an 'X' or 'O'
+            while theBoard[move] != ' ':
+                move = int(input('Choose an empty space (1-9): '))
+            theBoard[move] = turn
+
         printBoard(theBoard)
-        print('Turn for ' + turn + '. Move on which space?')
-        move = int(input())
-        theBoard[move] = turn
-        #gameWin(theBoard, turn)
 
         if ((theBoard[1] == turn and theBoard[2] == turn and theBoard[3] == turn) or # wins across the top
         (theBoard[4] == turn and theBoard[5] == turn and theBoard[6] == turn) or # wins across the middle row
@@ -33,6 +64,7 @@ def playGame():
             print ("\nPlayer %s wins!!!!\n" % turn)
             break
         else:
+            # if no one has won yet, switch turns
             if turn == 'X':
                 turn = 'O'
             else:
